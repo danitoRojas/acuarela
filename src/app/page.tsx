4dcd9c14403b6a00767b9fea197cda9fe44add56
Davis <davidusnayo265@gmail.com>
2025-10-31 08:50:44 -0400

    feat: se agregaron las secciones a page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import HomeHero from "@/components/sections/HomeHero";
+import Contacts from "@/components/sections/Contacts";
+import BlogSection from "@/components/sections/Blog";
+import NetworkStats from "@/components/sections/NetworkStats";
 
 export default function Page() {
   return (
     <>
-      <HomeHero />
+      <div className="flex flex-col gap-8 w-full">
+        <HomeHero />
+        <NetworkStats />
+        <BlogSection/>
+        <Contacts />
+      </div>
+
       {/* más secciones debajo */}
     </>
   );
