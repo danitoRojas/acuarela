184295adb053d42cbb3150de79b3952eef90a0c0
Davis <davidusnayo265@gmail.com>
2025-11-02 19:59:50 -0400

    style: mejoras en blog

--- src/components/sections/Blog.tsx
+++ src/components/sections/Blog.tsx
@@ -1,14 +1,14 @@
 export default function BlogSection() {
   const blogPosts = [
     {
-      image: "https://tse4.mm.bing.net/th/id/OIP.o5MTN9P9DjQC0Dt6vTp5xQHaEJ?rs=1&pid=ImgDetMain&o=7&rm=3",
+      image: "https://tse2.mm.bing.net/th/id/OIP.qIcdPQh0pLpyMAl-7IW0tQHaEK?rs=1&pid=ImgDetMain&o=7&rm=3",
       date: "August 19, 2025",
       category: "Educación",
       categoryColor: "bg-blue-200 text-blue-800",
       title: "Cómo motivar a tu hijo este regreso a clases: 7 consejos para acompañarlo con cariño y visión.",
     },
     {
-      image: "https://tse4.mm.bing.net/th/id/OIP.o5MTN9P9DjQC0Dt6vTp5xQHaEJ?rs=1&pid=ImgDetMain&o=7&rm=3",
+      image: "https://tse2.mm.bing.net/th/id/OIP.qIcdPQh0pLpyMAl-7IW0tQHaEK?rs=1&pid=ImgDetMain&o=7&rm=3",
       date: "July 21, 2025",
       category: "Noticias",
       categoryColor: "bg-gray-300 text-gray-800",
@@ -16,7 +16,7 @@ export default function BlogSection() {
         "La Participación de Nuestros Alumnos en el ISTE 2025: Un Escenario para Líderes en Tecnología e Innovación",
     },
     {
-      image: "https://tse4.mm.bing.net/th/id/OIP.o5MTN9P9DjQC0Dt6vTp5xQHaEJ?rs=1&pid=ImgDetMain&o=7&rm=3",
+      image: "https://tse2.mm.bing.net/th/id/OIP.qIcdPQh0pLpyMAl-7IW0tQHaEK?rs=1&pid=ImgDetMain&o=7&rm=3",
       date: "August 19, 2025",
       category: "Red de Colegios Semper Altius",
       categoryColor: "bg-blue-200 text-blue-800",
@@ -25,7 +25,7 @@ export default function BlogSection() {
   ]
 
   return (
-    <section className="py-16 px-8 relative overflow-hidden max-w-5/6 m-auto">
+    <section className="py-16 px-8 relative overflow-hidden w-[94%] md:max-w-5/6 m-auto">
       <div className="h-[400px] bg-[#3d4463] absolute top-0 left-0 w-full rounded-2xl overflow-hidden"></div>
       <div className="absolute left-0 top-1/2 -translate-y-1/2 w-24 h-32 bg-[#0066a1] rounded-tr-3xl rounded-br-3xl" />
       <div className="absolute -top-8 -right-8 w-32 h-32 bg-[#e67a6f] rounded-full" />
@@ -42,7 +42,7 @@ export default function BlogSection() {
           {blogPosts.map((post, index) => (
             <article
               key={index}
-              className="bg-white rounded-2xl overflow-hidden shadow-xl border border-gray-400 transition-colors"
+              className="bg-white rounded-2xl overflow-hidden shadow-xl border border-gray-300 transition-colors"
             >
               <div className="aspect-[4/3] overflow-hidden p-4">
                 <img src={post.image || "/placeholder.svg"} alt={post.title} className="w-full rounded-lg h-full object-cover" />
